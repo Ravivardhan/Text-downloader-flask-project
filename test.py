aaac74540bcb2f75421833150307e7f470b634be
unknown <ravivardhanbtechcse@gmail.com>
2021-06-14 12:23:57 +0530

    first commit

--- test.py
+++ test.py
@@ -1,7 +1,25 @@
-list1=[]
-def new():
-    global list1
-    list1=[1,2,3]
-print(list1)
-new()
-print(list1)
+no_of=input('enter number of elements')
+weights=[]
+weights=input("enter size of element").split()
+
+
+new=[]
+colors=0
+sol=True
+while sol==True:
+    high=weights[-1]
+    low=weights[0]
+
+    if low<=2*high:
+        ele=low+high
+        weights.remove(weights[-1])
+        weights.remove(weights[0])
+        new.append(ele)
+
+        weights.append(ele)
+        weights = sorted(weights)
+        colors+=1
+
+    if len(weights)==1:
+        sol=False
+print(colors)
